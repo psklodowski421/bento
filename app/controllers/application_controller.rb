1ed6bb311d444904a9bcaca6d5ead4ee432207b5
David Kewal <david.kewal@gmail.com>
2020-09-12 22:27:06 +0100

    Add http basic auth module to enable locking down non dev environments (#4)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 # Root Application Controller class.
 class ApplicationController < ActionController::Base
+  before_action :authenticate, if: -> { BasicAuthenticable.required? }
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      BasicAuthenticable.authenticate(username, password)
+    end
+  end
 end
